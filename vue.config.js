37dfd736f634a297ffe8fa53d791280d719b6eb5
Nayan Biswas <nayan32biswas@gmail.com>
2021-04-09 22:46:40 +0600

    update config and deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/hello-world/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  productionSourceMap: process.env.NODE_ENV != "production",
 };
