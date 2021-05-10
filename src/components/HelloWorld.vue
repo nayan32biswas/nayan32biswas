ca8844dda1403f8a5e8b49960fe0ee8dd84148d4
Nayan Biswas <nayan32biswas@gmail.com>
2021-05-10 18:33:45 +0600

    reinit vue2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -44,6 +44,14 @@
       </li>
       <li>
         <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
+          target="_blank"
+          rel="noopener"
+          >unit-jest</a
+        >
+      </li>
+      <li>
+        <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
           target="_blank"
           rel="noopener"
