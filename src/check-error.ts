98b4eb308f6e16c0dd77c5d679a68e4d7df1afa3
Nayan Biswas <nayan32biswas@gmail.com>
2021-11-15 22:38:50 +0600

    error push

---
+++ src/check-error.ts
@@ -0,0 +1,14 @@
+// import POST_CONTENT from "./store/postContent";
+// import POST_DATA from "./store/postData";
+
+// // const Message = require("./store/postData.ts");
+
+// (function () {
+//   if (POST_CONTENT?.length !== POST_DATA?.length) {
+//     throw new Error("POST_CONTENT.length !== POST_DATA.length");
+//   }
+// })();
+
+console.log("check-error.ts");
+
+// throw new Error("error");
